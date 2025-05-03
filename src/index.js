c55d893f540fd3fa37b7316a3d4acfa485235f95
elizaveta-tukailo <lizatukailo@gmail.com>
2025-05-03 18:30:21 +0300

    refactor: separate showing messages from function that gets the username

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import { greetUser } from './utils/greetUser.js';
+import * as readline from 'node:readline/promises';
+import { getUsername } from './utils/getUsername.js';
+import { showUserMessage } from './utils/showUserMessage.js';
+import { stdin as input, stdout as output } from 'node:process';
 
 const init = async () => {
-    greetUser();
+    const username = getUsername();
+    showUserMessage(username, "greeting");
+
+    //test work with interface
+    const rl = readline.createInterface({ input, output });
+    const answer = await rl.question('What do you think of Node.js? ');
+
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+
+    rl.close();
 }
 await init();
